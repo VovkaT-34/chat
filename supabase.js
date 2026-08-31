25707af2c934820c7cb27fa125f40d373cc590cb
VovkaT-34 <147396866+VovkaT-34@users.noreply.github.com>
2026-08-31 18:06:08 +0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,6 @@
-const SUPABASE_URL = "https://txqcnzmmhnmobrqiuhmu.supabase.co";
+const SUPABASE_URL = "https://sxkukrqjtgkxmzuzondm.supabase.co";
 
-//const SUPABASE_URL = "https://txqcnzmmhnmobrqiuhmu.supabase.co/rest/v1";
-
-const SUPABASE_KEY = "sb_publishable_ShcAZajiG3F9cuCTV2ZvEQ_pwO6f5A6";
-
+const SUPABASE_KEY = "sb_publishable_bepvJnr4yp-TUIyDK4Wnig_5qEhej3N";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
